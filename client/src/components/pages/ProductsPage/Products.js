074e84a5bdbad50c13de6d31229163612cdb08c9
Alex Gzyl <gzylalex@gmail.com>
2022-04-07 08:39:26 -0600

    productspage progress

---
+++ client/src/components/pages/ProductsPage/Products.js
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from "react"
+import styled from "styled-components";
+import {AiFillShopping} from "react-icons/ai"
+import Card from "./Card"
+//confirm react icons version before commit
+//confirm change to products model
+
+const Products = () => {
+    const [products, setProducts] = useState([])
+    //on mount query the products and store them in state
+    useEffect(() => {
+        fetch(`/products`)
+            .then((res) => res.json())
+            .then((json) => {
+            setProducts(json.data);
+            });
+    }, []);
+
+return (
+<>
+    <Title>
+    Products Page<AiFillShopping style={{color: "#45A29E", fontSize: "26px", marginLeft:"10px"}}/>
+    </Title>
+    <Cards>
+    {products.map((product)=>{
+    return <Card key ={product["_id"]} product = {product}></Card>
+    })}
+    </Cards>
+</>
+);
+};
+
+
+const Title = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items:center;
+    font-size: 26px;
+    margin-top:2%;
+`;
+
+const Cards = styled.div`
+display: flex;
+flex-wrap: wrap;
+align-items:center;
+justify-content: center;
+
+`;
+
+export default Products;
